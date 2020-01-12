46fea944f47e05a67b19b95c9cbb58e819f9088a
Graydee Chen <graydeec@gmail.com>
2020-01-12 18:14:06 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar';
+import Default from './components/Default';
+import Details from './components/Details';
+import ProductList from './components/ProductList';
+import Cart from './components/Cart'
+import Model from './components/Model'
+
+
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar/>
+      <Switch>
+        <Route exact path="/" component={ProductList} />
+        <Route path="/details" component={Details} />
+        <Route path="/cart" component={Cart} />
+        <Route component={Default} />
+      </Switch>
+      <Model/>
+    </React.Fragment>
+  );
+}
+
+export default App;
